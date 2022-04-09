25ac83eefc61395213ad03e1e5e49a8619ef5973
Vadim Alperovich <vadikalperovich@mail.ru>
2022-04-09 19:11:15 +0300

    refactor server

--- server/run.py
+++ server/run.py
@@ -4,27 +4,30 @@ import uvicorn
 from fastapi import FastAPI, Request
 
 
-import uvicorn
-
-
-app = FastAPI()
+app = FastAPI(title='The accent bot', description='English pronunciation scoring via chat-bot')
 
 TOKEN = os.getenv("TG_TOKEN", None)  # Telegram Bot API Key
 OWNER_CHAT_ID = os.getenv("OWNER_CHAT_ID", '<some-chat-id>')  # Telegram Chat ID
 
-async def sendTgMessage(message: str, chat_id: str):
-    print(message)
-    tg_msg = {"chat_id": chat_id, "text": message, "parse_mode": "Markdown"}
-    API_URL = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-    async with httpx.AsyncClient() as client:
-        resp = await client.post(API_URL, json=tg_msg)
-    print(resp.json())
 
-@app.post("/send/")
+@app.post("/set_webhook/")
+async def set_webhook(host: str):
+    url = f"https://api.telegram.org/{TOKEN}/setWebhook?url={host}/api/"
+    async with httpx.AsyncClient() as client:
+        resp = await client.post(url)
+    return resp
+
+
+@app.post("/api/send/")
 async def send(message: str = '*Hello*, _world_!', chat_id: str = OWNER_CHAT_ID):
-    await sendTgMessage(message, chat_id)
+    await tg_send_message(message, chat_id)
     return {'status': message}
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)#, reload=True)
+async def tg_send_message(message: str, chat_id: str):
+    print(message)
+    tg_msg = {"chat_id": chat_id, "text": message, "parse_mode": "Markdown"}
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    async with httpx.AsyncClient() as client:
+        resp = await client.post(url, json=tg_msg)
+    print(resp.json())
