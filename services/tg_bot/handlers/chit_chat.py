a64aaf2ee76d662485efb8bff0a809c957ceeffd
Vadim Alperovich <vadikalperovich@mail.ru>
2022-05-19 01:49:33 +0300

    chit-chat added :fire:

--- services/tg_bot/handlers/chit_chat.py
+++ services/tg_bot/handlers/chit_chat.py
@@ -13,7 +13,10 @@ async def process_play_command(message: types.Message):
 @dp.message_handler(content_types=ContentType.TEXT, state='*')
 async def echo(message: types.Message):
     await types.ChatActions.typing()
-    await message.answer(f'{message.from_user.first_name} said: {message.text}')
+    # Get a response to the input text 'I would like to book a flight.'
+    response = chit_chat.get_response(message)
+    print(response)
+    await message.answer(str(response))
 
 
 @dp.message_handler(content_types=ContentType.VOICE, state='*')
@@ -22,7 +25,33 @@ async def process_some_audio(message: types.Voice):
     duration = message.to_python()['voice']['duration']
     await message.reply(md.text(md.text(f"Wow\! {duration} seconds long voice msg\!"),
                                 md.text("What are you trying to say?"),
-                                sep="\n"))
+                                sep="\n"), parse_mode=types.ParseMode.MARKDOWN_V2)
     DialogState.offer_to_start.set()
     markup = await get_offer_start_keyboard()
     await message.answer("Do you wanna score your english accent?", reply_markup=markup)
+
+
+from chatterbot import ChatBot
+# from chatterbot.trainers import ListTrainer
+from chatterbot.trainers import ChatterBotCorpusTrainer
+
+# Create a new chat bot named Charlie
+chit_chat = ChatBot('Small-Talker',
+                    read_only=True,
+                    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+                    logic_adapters=[
+                        'chatterbot.logic.BestMatch',
+                        'chatterbot.logic.MathematicalEvaluation',
+                    ]
+                    )
+trainer = ChatterBotCorpusTrainer(chit_chat.storage)
+trainer.train(
+    "chatterbot.corpus.english.conversations",
+    "chatterbot.corpus.english.emotion",
+    "chatterbot.corpus.english.greetings",
+    "chatterbot.corpus.english.humor",
+    "chatterbot.corpus.english.movies",
+    "chatterbot.corpus.english.trivia",
+    "chatterbot.corpus.english.botprofile",
+    "chatterbot.corpus.english.ai",
+)
