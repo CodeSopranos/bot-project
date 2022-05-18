a87b995ef824ea1de37e5c738569e8bdf594a33f
Vadim Alperovich <vadikalperovich@mail.ru>
2022-05-19 01:49:57 +0300

    dummy end-points added

--- server/run.py
+++ server/run.py
@@ -12,12 +12,12 @@ TOKEN = os.getenv("TG_TOKEN", None)  # Telegram Bot API Key
 OWNER_CHAT_ID = os.getenv("OWNER_CHAT_ID", '<some-chat-id>')  # Telegram Chat ID
 
 
-@app.get("/ping/")
+@app.get("/ping/", tags=["Utils"])
 async def set_webhook():
     return {"message": "PONG."}
 
 
-@app.post("/set_webhook/")
+@app.post("/set_webhook/", tags=["Utils"])
 async def set_webhook(host: str):
     url = f"https://api.telegram.org/{TOKEN}/setWebhook?url={host}/api/"
     async with httpx.AsyncClient() as client:
@@ -25,20 +25,30 @@ async def set_webhook(host: str):
     return resp
 
 
-@app.post("/api/send/")
+@app.post("/api/send/", tags=["Utils"])
 async def send(message: str = '*Hello*, _world_!', chat_id: str = OWNER_CHAT_ID):
     await tg_send_message(message, chat_id)
     return {'status': message}
 
 
-@app.post("/api/predict/")
+@app.post("/api/predict/", tags=["Scoring"])
 async def predict(audio: str = 'audio'):
     score = dummy_model.predict()
     most_relevant = ['en', 'ru', 'de']
     return {'score': score, 'most_relevant': most_relevant}
 
 
-@app.get("/api/get_sample/")
+@app.post("/api/audio/denoise/", tags=["Audio"])
+async def denoise_audio(audio: str = 'audio'):
+    return {'status': "OK"}
+
+
+@app.post("/api/audio/accent-recognition/", tags=["Audio"])
+async def denoise_audio(audio: str = 'audio'):
+    return {'status': "OK"}
+
+
+@app.get("/api/get_sample/", tags=["Scoring"])
 async def get_sample():
     sentence = dummy_model.get_sample()
     return {'sample': sentence}
